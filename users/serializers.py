e3b524b7c7ccbf2e1ccefe76d1382777ad43810c
hj <darkkong88@gmail.com>
2020-10-23 23:04:11 +0900

    #2.16 Searching Part Two

--- users/serializers.py
+++ users/serializers.py
@@ -2,19 +2,6 @@ from rest_framework import serializers
 from .models import User
 
 
-# class RelatedUserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = (
-#             "username",
-#             "first_name",
-#             "last_name",
-#             "email",
-#             "avatar",
-#             "superhost",
-#         )
-
-
 class UserSerializer(serializers.ModelSerializer):
 
     password = serializers.CharField(write_only=True)
